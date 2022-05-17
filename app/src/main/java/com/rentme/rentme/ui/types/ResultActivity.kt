79e263b9f4ea2fc5ab6c9a94a268539ac83ae438
Tohir <trakhmatullayev@gmail.com>
2022-05-17 18:10:07 +0500

    Types pages begin

---
+++ app/src/main/java/com/rentme/rentme/ui/types/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.rentme.rentme.ui.types
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rentme.rentme.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
