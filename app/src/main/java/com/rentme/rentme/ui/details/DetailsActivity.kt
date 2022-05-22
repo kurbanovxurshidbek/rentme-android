f3ddaaa8f002d066625cf800c29df7d6e82a333d
Tohir <trakhmatullayev@gmail.com>
2022-05-23 01:09:39 +0500

    Types pages begin

--- app/src/main/java/com/rentme/rentme/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/rentme/rentme/ui/details/DetailsActivity.kt
@@ -1,5 +1,6 @@
 package com.rentme.rentme.ui.details
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,6 +9,7 @@ import com.rentme.rentme.R
 import com.rentme.rentme.adapter.DetailPhotoAdapter
 import com.rentme.rentme.databinding.ActivityDetailsBinding
 import com.rentme.rentme.model.DetailPhoto
+import com.rentme.rentme.ui.location.SelectLocationActivity
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -34,15 +36,20 @@ class DetailsActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.llLocationDetails.setOnClickListener {
+            intent = Intent(this, SelectLocationActivity::class.java)
+            startActivity(intent)
+        }
+
 
     }
 
     private fun getAllDetailPhoro(){
         val items:ArrayList<DetailPhoto> = ArrayList()
+        items.add(DetailPhoto(R.drawable.im_tesla_model3))
+        items.add(DetailPhoto(R.drawable.im_mersades))
         items.add(DetailPhoto(R.drawable.im_malibu))
-        items.add(DetailPhoto(R.drawable.im_malibu))
-        items.add(DetailPhoto(R.drawable.im_malibu))
-        items.add(DetailPhoto(R.drawable.im_malibu))
+        items.add(DetailPhoto(R.drawable.im_tesla_model3))
 
         adapter.sumbitData(items)
     }
