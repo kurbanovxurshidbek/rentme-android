64d36782aff226c9c3b2db18ae7bd0f1989bc455
sherzoddbekk <jurabekovsherzod07@gmail.com>
2022-05-23 01:24:11 +0500

    splashhhh is finished

--- app/src/main/java/com/rentme/rentme/ui/details/information/InformationActivity.kt
+++ app/src/main/java/com/rentme/rentme/ui/details/information/InformationActivity.kt
@@ -1,12 +1,31 @@
 package com.rentme.rentme.ui.details.information
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rentme.rentme.MainActivity
 import com.rentme.rentme.R
+import com.rentme.rentme.databinding.ActivityInformationBinding
+import com.rentme.rentme.databinding.ActivityLogInBinding
 
 class InformationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInformationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_information)
+        binding = ActivityInformationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.registration.setOnClickListener {
+            startMainActivity()
+        }
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
