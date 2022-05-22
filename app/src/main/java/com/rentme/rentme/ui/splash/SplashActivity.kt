d4caec5fa6917d76422fce90c3319d62ec34467e
sherzoddbekk <jurabekovsherzod07@gmail.com>
2022-05-22 23:43:48 +0500

    splashh is finished

--- app/src/main/java/com/rentme/rentme/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/rentme/rentme/ui/splash/SplashActivity.kt
@@ -1,12 +1,37 @@
 package com.rentme.rentme.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.rentme.rentme.R
+import com.rentme.rentme.ui.logIn.LogInActivity
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_SCREEN = 5000
+    var bottom: Animation? = null
+    lateinit var image: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        initView()
+    }
+
+    private fun initView() {
+        //Animation
+        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+
+        //Hooks
+        image = findViewById(R.id.car_running)
+        image.setAnimation(bottom)
+        Handler().postDelayed({
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_SCREEN.toLong())
     }
 }
