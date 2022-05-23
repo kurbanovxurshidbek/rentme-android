f64075a5fcfd5f36e2297649d56de10b89c32b5c
Azizjon <azizjonsheronov2607@gmail.com>
2022-05-23 14:26:34 +0500

    Home page ui

--- app/src/main/java/com/rentme/rentme/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/rentme/rentme/ui/splash/SplashActivity.kt
@@ -1,12 +1,16 @@
 package com.rentme.rentme.ui.splash
 
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.rentme.rentme.R
 import com.rentme.rentme.databinding.ActivitySplashBinding
 import com.rentme.rentme.ui.localization.LocalizationActivity
@@ -19,6 +23,9 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setLightStatusBar()
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN or WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
         initView()
     }
 
@@ -35,5 +42,20 @@ class SplashActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }, SPLASH_SCREEN.toLong())
+    }
+
+    fun setLightStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.statusBarColor = Color.WHITE
+
+            var flags: Int = this.window.decorView.systemUiVisibility
+            flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR // add LIGHT_STATUS_BAR to flag
+            this.window.decorView.systemUiVisibility = flags
+            this.window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+
+        }
     }
 }
