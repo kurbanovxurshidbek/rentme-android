285ab2fd03424bd8e30725f32ada8b85bcc6e411
Azizjon <azizjonsheronov2607@gmail.com>
2022-05-27 14:41:42 +0500

    Home page ui

--- app/src/main/java/com/rentme/rentme/ui/location/SelectLocationFragment.kt
+++ app/src/main/java/com/rentme/rentme/ui/location/SelectLocationFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.rentme.rentme.R
 import com.rentme.rentme.databinding.FragmentSelectLocationBinding
 
@@ -20,6 +21,14 @@ class SelectLocationFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentSelectLocationBinding.inflate(LayoutInflater.from(container!!.context), container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.llCurrentLocation.setOnClickListener {
+            findNavController().navigate(R.id.mapsFragment)
+        }
     }
 
     override fun onDestroy() {
