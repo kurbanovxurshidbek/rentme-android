ac05c6ac24b1f1492b88909986ddf4ec8f2d9e48
Developer <Jaxadev>
2022-06-08 19:35:36 +0500

    AddedBottomSheet12

--- app/src/main/java/com/rentme/rentme/data/remote/ApiService.kt
+++ app/src/main/java/com/rentme/rentme/data/remote/ApiService.kt
@@ -1,7 +1,12 @@
 package com.rentme.rentme.data.remote
 
 import com.rentme.rentme.model.MainPage
-import retrofit2.http.GET
+import com.rentme.rentme.model.UploadAdvertisement
+import com.rentme.rentme.model.UploadAdvertisementResp
+import com.rentme.rentme.model.base.BaseResponse
+import com.rentme.rentme.model.base.BaseResponseList
+import retrofit2.http.*
+import java.io.File
 
 interface ApiService {
 
