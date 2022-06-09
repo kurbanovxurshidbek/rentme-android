5f87848b833ea2bf984f043157b4df2f16c47030
Developer <Jaxadev>
2022-06-09 20:21:03 +0500

    AddedBottomSheet12

---
+++ app/src/main/java/com/rentme/rentme/adapter/SearchAdapter.kt
@@ -0,0 +1,73 @@
+package com.rentme.rentme.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.rentme.rentme.data.local.entity.ModelsListEntity
+import com.rentme.rentme.databinding.ItemSearchHistoryBinding
+
+class SearchAdapter : ListAdapter<ModelsListEntity,SearchAdapter.VH>(ITEM_DIF), Filterable {
+
+    var carNames: List<ModelsListEntity> = ArrayList()
+
+    inner class VH(private val binding: ItemSearchHistoryBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ModelsListEntity) {
+            binding.tvHistoryLocation.text = item.modelName
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        return VH(
+            ItemSearchHistoryBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) = holder.bind(getItem(position))
+
+    fun setData(list: List<ModelsListEntity>) {
+        this.carNames = list
+        submitList(list)
+    }
+
+    companion object {
+        private val ITEM_DIF = object : DiffUtil.ItemCallback<ModelsListEntity>() {
+            override fun areItemsTheSame(oldItem: ModelsListEntity, newItem: ModelsListEntity): Boolean =
+                oldItem.modelName == newItem.modelName
+
+            override fun areContentsTheSame(oldItem: ModelsListEntity, newItem: ModelsListEntity): Boolean =
+                oldItem.modelName == newItem.modelName
+        }
+    }
+
+    override fun getFilter(): Filter {
+        return customFilter
+    }
+
+    private val customFilter = object : Filter() {
+        override fun performFiltering(p0: CharSequence?): FilterResults {
+            val filterableList = mutableListOf<ModelsListEntity>()
+            if (p0 == null || p0.isEmpty()) {
+                filterableList.addAll(carNames)
+            } else {
+                for (item in carNames) {
+                    if (item.modelName.lowercase().contains(p0.toString().lowercase())) {
+                        filterableList.add(item)
+                    }
+                }
+            }
+
+            val results = FilterResults()
+            results.values = filterableList
+            return results
+        }
+
+        override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+            submitList(p1?.values as MutableList<ModelsListEntity>)
+        }
+
+    }
+}
