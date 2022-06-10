6d3833cd27d80caa383fd35c985f7e2ded7d1b39
Azizjon <azizjonsheronov2607@gmail.com>
2022-06-10 18:38:00 +0500

    ApiService updated

--- app/src/main/java/com/rentme/rentme/utils/Extensions.kt
+++ app/src/main/java/com/rentme/rentme/utils/Extensions.kt
@@ -6,15 +6,16 @@ import java.util.*
 
 object Extensions {
     @SuppressLint("SimpleDateFormat")
-    fun toTimestamp(date: String): Long{
-
-        val timestamp = SimpleDateFormat("yyyy-MM-DD hh:mm:ss").parse(date)
+    fun toTimestamp(date: String, format: String): Long{
+//       format = "dd-MM-yyyy"
+        val timestamp = SimpleDateFormat(format).parse(date)
         return timestamp!!.time
     }
 
     @SuppressLint("SimpleDateFormat")
-    fun toDateFromTimestamp(timestamp: Long): String{
-        val mFormat = SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'", Locale.ENGLISH)
+    fun toDateFromTimestamp(timestamp: Long, format: String): String{
+//        format = "yyyy-MM-dd'T'hh:mm:ss.SSS'Z'"
+        val mFormat = SimpleDateFormat(format, Locale.ENGLISH)
         return mFormat.format(Date(timestamp))
     }
 }
