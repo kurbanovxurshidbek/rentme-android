0e02a93994acd9e61abe2dd1418e650070f7ec30
Mardiyev Abbos <abbosmardiev1999@gmail.com>
2022-06-14 16:11:05 +0500

    Changed 21

--- app/src/main/java/com/rentme/rentme/ui/main/upload/FeatureFragment.kt
+++ app/src/main/java/com/rentme/rentme/ui/main/upload/FeatureFragment.kt
@@ -35,8 +35,6 @@ import com.sangcomz.fishbun.FishBun
 import com.sangcomz.fishbun.adapter.image.impl.GlideAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-import okhttp3.MediaType
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
